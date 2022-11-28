73f25ba6a8c3103a2417aa7dc05619ce22fe9960
Guido Dinello <guido.dinello@gmail.com>
2022-11-28 01:06:02 -0300

    nueva presentacion csv

--- Statistics.py
+++ Statistics.py
@@ -15,68 +15,72 @@ class Statistics:
         self.conf_img_path = configuration_image_path
         return
 
-    def parametric_evaluation(self, n_seed=3):    
-        results = {}
+    def informal_evaluation(self, best_config, n_seed=30):
+        p = {
+            "MU" : [50, 100],
+            "select_operator" : [
+               [tools.selTournament, 3], [tools.selBest],  [tools.selRoulette]
+            ]
+        }
+        return
+
+    def greedy_evaluation(self, n_seed=30):
+        return
+
+    def parametric_evaluation(self, n_seed=30):    
 
         # se setea la imagen que se va a utilizar para la configuracion
         self.eac.evolutionary_algorithm.image_processor.img_in_dir = self.conf_img_path
         self.eac.evolutionary_algorithm.load_image()
 
         p = {
-            # le saque el 1 a CXPB me daba problema assert CXPB+MUTPB<=1
             "CXPB" : [0.8, 0.9],
             "MUTPB" : [0.01, 0.05, 0.1],
-            "select_operator" : [
-               [tools.selTournament, 3], [tools.selBest],  [tools.selStochasticUniversalSampling]
-            ]
-            # "CXPB" : [0.8, 0.9],
-            # "MUTPB" : [0.1],
-            # "select_operator" : [
-            #     [tools.selBest]
-            # ]
         }
         ortogonal_combinations = list(product(p["CXPB"], p["MUTPB"], p["select_operator"]))
+        values = []
 
         # cada configuracion parametrica
-        for config in ortogonal_combinations:
+        for cxpb, mutpb, select_operator in ortogonal_combinations:
 
-                cxpb, mutpb, select_operator = config
+            if len(select_operator) == 2:
+                self.eac.deap_configurer.__getattribute__("toolbox").register("select", select_operator[0], tournsize=select_operator[1])
+            else:
+                self.eac.deap_configurer.__getattribute__("toolbox").register("select", select_operator[0])
 
-                if len(select_operator) == 2:
-                    self.eac.deap_configurer.__getattribute__("toolbox").register("select", select_operator[0], tournsize=select_operator[1])
-                else:
-                    self.eac.deap_configurer.__getattribute__("toolbox").register("select", select_operator[0])
-
-                self.eac.deap_configurer.__setattr__("CXPB", cxpb)
-                self.eac.deap_configurer.__setattr__("MUTPB", mutpb)
-
-                comb_name = f"{cxpb}_{mutpb}_{select_operator[0].__name__}"
-                results[comb_name] = {}
-
-                best_execution_fitness = []
-
-                # para cada seed
-                for s in range(1, n_seed):
-                    # ejecutar el algoritmo
-                    self.eac.deap_configurer.__setattr__('seed', s)
-
-                    # si no salta error de pool not running
-                    self.eac.deap_configurer.register_parallelism()
-
-                    # devuelve lista de min fitness en cada generacion
-                    best_fitness_per_gen = self.eac.run(show_res=False)
-
-                    # se guarda el min fitness de una ejecucion. ejecucion = conf_alg + seed
-                    best_execution_fitness.append(min(best_fitness_per_gen))
+            self.eac.deap_configurer.__setattr__("CXPB", cxpb)
+            self.eac.deap_configurer.__setattr__("MUTPB", mutpb)
 
 
-                # se guardan los valores obtenidos para la configuracion
-                results[comb_name]["best_historical_fitness"] = min(best_execution_fitness)
-                results[comb_name]["avg_best_fitness"] = np.mean(best_execution_fitness)
-                results[comb_name]["std_fitness"] = np.std(best_execution_fitness)
+            best_execution_fitness = []
 
-                pd.DataFrame(results).to_csv(f"results/resultados.csv")
+            # para cada seed
+            for s in range(1, n_seed):
+                # ejecutar el algoritmo
+                self.eac.deap_configurer.__setattr__('seed', s)
 
+                # si no salta error de pool not running
+                self.eac.deap_configurer.register_parallelism()
+
+                # devuelve lista de min fitness en cada generacion
+                best_fitness_per_gen = self.eac.run(show_res=False)
+
+                # se guarda el min fitness de una ejecucion. ejecucion = conf_alg + seed
+                best_execution_fitness.append(min(best_fitness_per_gen))
+
+            # se guardan los valores obtenidos para la configuracion
+            values.append([
+                cxpb, mutpb, select_operator[0].__name__, min(best_execution_fitness), 
+                np.mean(best_execution_fitness), np.std(best_execution_fitness),
+                kstest(best_execution_fitness, "norm", alternative='two-sided').pvalue
+            ])
+
+        header = [
+            "CXPB", "MUTPB", "select_operator", "best_historical_fitness", 
+            "avg_best_fitness", "std_fitness", "p-value"
+        ]
+        pd.DataFrame(values, columns=header).to_csv(f"results/resultados.csv", index=False)
+    
         return
 
     # otras cosas posibles a agregar:
@@ -96,24 +100,33 @@ class Statistics:
         
 
         """
-        timestamps = {}
+        values = []
 
         for i in range(1,os.cpu_count()+1):
             self.eac.deap_configurer.__setattr__('cpu_count', i)
             start = time.time()
-            self.eac.run()
+            self.eac.run(show_res=False)
             end = time.time()
 
-            timestamps[i] = {}
-            timestamps[i]["time"] = end - start
-            timestamps[i]["speedup"] = timestamps[1]["time"] / timestamps[i]["time"]
-            timestamps[i]["efficiency"] = timestamps[i]["speedup"] * (1/i)
+            time_i = end - start
+            time_1 = values[0][1] if i!=1 else time_i
+            speedup = time_1 / time_i
 
-        pd.DataFrame(timestamps).to_csv(f"results/time.csv")
+            values.append([i, time_i, speedup, speedup * (1/i)])
+
+        header = ["CPU", "time", "speedup", "efficiency"]
+        pd.DataFrame(values, columns=header).to_csv(f"results/time.csv", index=False)
 
         return 
 
     def normality_test(self, sample): 
+        """
+        Suppose we wish to test the null hypothesis,
+            N0: the sample is distributed according to the standard normal. 
+        We choose a confidence level of 95%; 
+        1) If the p-value is less than our threshold (0.05), we reject the null hypothesis.
+        2) If the p-value is greater than our threshold (0.05), we fail to reject the null hypothesis.
+        """
 
         bnp = np.array(sample)
         bnp = bnp - bnp.mean()
